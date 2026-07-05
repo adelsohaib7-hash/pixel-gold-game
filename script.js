6791efdc99db88347c1939a538e32b932316d0fa
adelsohaib7-hash <adelsohaib7@gmail.com>
2026-07-05 15:36:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let totalPG = Number(localStorage.getItem("totalPG")) || 39;
 let miningPG = Number(localStorage.getItem("miningPG")) || 0;
 
-const ratePerSecond = 1.4722 / (100 * 3600);
-
 const balance = document.getElementById("pgBalance");
 const rate = document.getElementById("rate");
 const claimBtn = document.getElementById("claimBtn");
+const circle = document.querySelector(".gold-circle");
+
+// 100 PG في الساعة
+const ratePerSecond = 100 / 3600;
 
 function saveData() {
     localStorage.setItem("totalPG", totalPG);
@@ -16,20 +18,24 @@ function updateScreen() {
     balance.innerText = "PG " + totalPG.toFixed(2);
     rate.innerText = miningPG.toFixed(4);
 
-    if (miningPG >= 10) {
-        claimBtn.disabled = false;
-    } else {
-        claimBtn.disabled = true;
-    }
+    claimBtn.disabled = miningPG < 10;
 
     saveData();
 }
 
+// التعدين التلقائي
 setInterval(() => {
     miningPG += ratePerSecond;
     updateScreen();
 }, 1000);
 
+// زيادة بالنقر على الدائرة
+circle.addEventListener("click", () => {
+    miningPG += 1;
+    updateScreen();
+});
+
+// استلام الأرباح
 claimBtn.addEventListener("click", () => {
     if (miningPG >= 10) {
         totalPG += miningPG;
